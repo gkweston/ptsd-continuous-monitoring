734680984930fba10591fc946d26741664ce9c60
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2020-02-17 11:33:50 -0600

    Update kalman_imputation.r

--- kalman_imputation.r
+++ kalman_imputation.r
@@ -8,9 +8,9 @@ library(imputeTS)
 
 ###Due to some reasons it cannot impute around 10 files. Those files are implemented separately
 i<- 1
-p<- list.files("/Users/gkweston/git/ptsd-continuous-monitoring/data/prep_output/P13FIXIMP/", pattern="*.csv", full.names=TRUE)
+p<- list.files("<path/to/file>", pattern="*.csv", full.names=TRUE)
 
-setwd("/Users/gkweston/git/ptsd-continuous-monitoring/data/prep_output/P13FIXIMP/")
+setwd("<path/to/wd>")
 for (i in 1:98) {
   #reading in data
   df<- fread(filename <- p[[i]])
